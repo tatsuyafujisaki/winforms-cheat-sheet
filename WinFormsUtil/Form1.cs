c8fdb2c5c86f409578874f4a620b5a9805bfb0e0
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-06-30 00:47:19 +0900

    First commit

---
+++ WinFormsUtil/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsUtil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WinFormsUtil/U.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsUtil
+{
+    internal static class U
+    {
+        // Explanatory wrapper
+        internal static void SetTextToClipboard(string s)
+        {
+            Clipboard.SetText(s);
+        }
+
+        // Mark Main method with STAThread or ThreadStateException will be thrown.
+        internal static string OpenFileDialog()
+        {
+            var ofd = new OpenFileDialog
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+                RestoreDirectory = true
+            };
+
+            return ofd.ShowDialog() == DialogResult.OK ? ofd.FileName : null;
+        }
+
+        // Mark Main method with STAThread or ThreadStateException will be thrown.
+        internal static string SaveFileDialog()
+        {
+            var sfd = new SaveFileDialog
+            {
+                Filter = @"All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+                RestoreDirectory = true
+            };
+
+            return sfd.ShowDialog() == DialogResult.OK ? sfd.FileName : null;
+        }
+    }
+}
