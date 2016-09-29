b3aae705b28513c55904814ef6985daff6f369c1
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-09-30 00:45:53 +0900

    Add ToastForm.cs

---
+++ WinFormsUtil/ToastForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsUtil
+{
+    public partial class ToastForm : Form
+    {
+        public ToastForm(string s)
+        {
+            InitializeComponent();
+
+            label1.Text = s;
+        }
+
+        public void CloseForm()
+        {
+            if (InvokeRequired)
+            {
+                Invoke((Action)Close);
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+}
