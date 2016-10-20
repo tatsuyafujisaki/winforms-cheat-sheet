0ac697cc6ea1fad4c9ae1d840bee3c04af5a961c
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-10-20 23:19:01 +0900

    Add Load in comment

--- WinFormsUtil/Form1.cs
+++ WinFormsUtil/Form1.cs
@@ -7,6 +7,13 @@ namespace WinFormsUtil
         internal Form1()
         {
             InitializeComponent();
+
+            //if (SomethingWrong)
+            //{
+            //    // Exit after constructor.
+            //    // Note that there is no way to exit in the constructor.
+            //    Load += (sender, e) => Close();
+            //}
         }
 
         void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
