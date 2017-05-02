60b8b1e187b10137afaabbd19539665ec9ad81c2
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2017-05-03 00:42:30 +0900

    Update .NET Framework to v4.7

--- WinFormsCheatSheet/Properties/AssemblyInfo.cs
+++ WinFormsCheatSheet/Properties/AssemblyInfo.cs
@@ -11,5 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("5721306b-7006-425d-8cdb-7a4700412dcf")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
